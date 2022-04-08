975fb7a0d184b8ec857d6936fd1599f5670e76a1
Davin Clark <davinic2@illinois.edu>
2022-04-08 02:54:47 -0500

    resume

--- src/DownloadBanner.jsx
+++ src/DownloadBanner.jsx
@@ -1,13 +1,17 @@
 import downloadButton from "/photos/downloadButton.jpg";
+import CloudBackground from "/photos/cloud_background.jpg"
+
 
 export default function DownloadBanner() {
 
     return (
-        <div className="flex flex-col items-center p-5 border-2 border-dashed">
+        <div className="flex flex-col items-center p-5 border-2 border-dashed bg-white">
             <p className="text-2xl">See my resume in 3 easy steps!</p>
-            <button className="">
-                <img src={downloadButton} alt="downloadButton" className="h-32" />
-            </button>
+            <a href='/downloads/resume.pdf' className="">
+                <button className="">
+                    <img src={downloadButton} alt="downloadButton" className="h-32" />
+                </button>
+            </a>    
             <p className="text-2xl">1. Click the button to download my resume!</p>
             <p className="text-2xl">2. Download Adobe Acrobat Reader DC software</p>
             <p className="text-2xl">3. Enjoy!</p>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import Me from './Me'
 import DownloadBanner from './DownloadBanner'
-// import CloudBackground from "/photos/cloud_background.jpg"
 import About from './About'
 
 import DavinClark from '/text_art/davin_clark.png';
@@ -10,7 +9,7 @@ import MySuper from '/text_art/my_super.png';
 function Home() {
 
   return (
-    <div className="p-10 border-2 border-red-500 bg-gradient-to-r from-purple-500 to-pink-500 ">
+    <div className="p-10 border-2 border-red-500 bg-gradient-to-br from-purple-500 to-pink-400 ">
       <img src={DavinClark} alt="Davin Clark" className="w-full" />
       <img src={MySuper} alt="Davin Clark" className="w-full" />
       <Me />
